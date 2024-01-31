dea2220846446a89f841d78f36add6ae29402984
FssAy <49795742+FssAy@users.noreply.github.com>
2024-01-31 17:51:49 +0100

    Fix `create_post` call

--- src/database/calls.rs
+++ src/database/calls.rs
@@ -3,12 +3,12 @@ use crate::handler::api::error::{api_error, ApiError};
 use super::*;
 
 impl Database {
-    pub async fn create_post(&self, content: String) -> Result<String, ApiError> {
+    pub async fn create_post(&self, content: impl AsRef<str>) -> Result<String, ApiError> {
         let post_id = self
             .query(surql::CREATE_POST)
-            .bind(("content", content))
+            .bind(("content", content.as_ref()))
             .await?
-            .take::<Option<Thing>>(0usize)?
+            .take::<Option<Thing>>((0, "id"))?
             .ok_or_else(|| api_error!(DatabaseError))?;
 
         Ok(post_id.id.to_raw())
