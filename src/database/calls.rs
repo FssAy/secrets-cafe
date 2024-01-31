9af38d75fdfceaa9017d021ab03303a3c071adee
FssAy <49795742+FssAy@users.noreply.github.com>
2024-01-31 17:17:06 +0100

    Fix return type and add error handling for `create_post` call

--- src/database/calls.rs
+++ src/database/calls.rs
@@ -1,15 +1,16 @@
+use surrealdb::sql::Thing;
+use crate::handler::api::error::{api_error, ApiError};
 use super::*;
 
 impl Database {
-    // todo: add error handling
-    // todo: return id
-    pub async fn create_post(&self, content: String) {
-        let _ = self
+    pub async fn create_post(&self, content: String) -> Result<String, ApiError> {
+        let post_id = self
             .query(surql::CREATE_POST)
             .bind(("content", content))
-            .await
-            .unwrap()
-            .check()
-            .unwrap()
+            .await?
+            .take::<Option<Thing>>(0usize)?
+            .ok_or_else(|| api_error!(DatabaseError))?;
+
+        Ok(post_id.id.to_raw())
     }
 }
