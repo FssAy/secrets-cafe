2361ca2e5672c70d0a9b0c1d283d538b6cf408d7
FssAy <49795742+FssAy@users.noreply.github.com>
2024-02-02 12:10:27 +0100

    Improve ModTier conversions

--- src/database/types/tier.rs
+++ src/database/types/tier.rs
@@ -9,18 +9,43 @@ pub enum ModTier {
     Remover = 1,
     /// Has full permissions to do everything.
     Admin = 2,
+    // if adding tier above the last one make sure to update Self::is_raw_in_bounds
     /// Same as admin, but also have access to unsafe features.
     Dev = 3,
 }
 
+impl ModTier {
+    fn is_raw_in_bounds(value: u8) -> bool {
+        if value <= ModTier::Dev as u8 {
+            true
+        } else {
+            false
+        }
+    }
+}
+
 impl From<u8> for ModTier {
     fn from(value: u8) -> Self {
-        if value > ModTier::Dev as u8 {
-            Self::default()
-        } else {
+        if Self::is_raw_in_bounds(value) {
             unsafe {
                 std::mem::transmute(value)
             }
+        } else {
+            Self::default()
+        }
+    }
+}
+
+impl TryFrom<&String> for ModTier {
+    type Error = ();
+
+    fn try_from(value: &String) -> Result<Self, Self::Error> {
+        let value = u8::from_str_radix(value, 10).map_err(|_| ())?;
+
+        if Self::is_raw_in_bounds(value) {
+            Ok(Self::from(value))
+        } else {
+            Err(())
         }
     }
 }
