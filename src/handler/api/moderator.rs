15352a8b915c6b2a956b0cc2046e7ff784854421
FssAy <49795742+FssAy@users.noreply.github.com>
2024-02-14 00:42:11 +0100

    Add functionality to change your own password as a mod

--- src/handler/api/moderator.rs
+++ src/handler/api/moderator.rs
@@ -1,5 +1,6 @@
 use hyper::Method;
 use crate::database::Database;
+use crate::database::types::{SessionToken, TokenPack};
 use crate::handler::api::error::ApiError;
 use super::*;
 
@@ -29,6 +30,40 @@ impl Moderator {
                 Ok(Res::new(Full::new(Bytes::from(
                     serde_json::to_string(&session).unwrap()
                 ))))
+            }
+            &Method::PATCH => {
+                let headers = req.headers();
+
+                let session = headers
+                    .get("session")
+                    .map(|value| value.to_str().ok())
+                    .ok_or_else(|| api_error!(InvalidHeader))?
+                    .ok_or_else(|| api_error!(InvalidHeader))?;
+
+                let pass = headers
+                    .get("pass")
+                    .map(|value| value.to_str().ok())
+                    .ok_or_else(|| api_error!(InvalidHeader))?
+                    .ok_or_else(|| api_error!(InvalidHeader))?;
+
+                let new_pass = headers
+                    .get("new_pass")
+                    .map(|value| value.to_str().ok())
+                    .ok_or_else(|| api_error!(InvalidHeader))?
+                    .ok_or_else(|| api_error!(InvalidHeader))?;
+
+                let token = SessionToken::verify(
+                    TokenPack::unpack(session.to_string())?
+                ).await?;
+
+                let db: Database = Database::get().await.unwrap();
+                db.update_mod_password(
+                    token.user_id,
+                    pass,
+                    new_pass
+                ).await?;
+
+                Ok(SingleResponse::Ok.as_res())
             }
             _ => Err(api_error!(MethodNotSupported))
         }
