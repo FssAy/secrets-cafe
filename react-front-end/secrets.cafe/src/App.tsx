6767c133d726959f3b11f06690a806c2f0e0c1f1
Yuya Iwatsuki <spidywebcoder@gmail.com>
2024-04-12 14:50:03 +0900

    add routes on App.tsx

--- react-front-end/secrets.cafe/src/App.tsx
+++ react-front-end/secrets.cafe/src/App.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/landingPage";
-import DonatePage from "./pages/donatePage";
-import DevelopPage from "./pages/developPage";
+import HomeContent from "./pages/HomePage";
+import DonatePage from "@/pages/DonatePage";
+import DevelopPage from "@/pages/DevelopPage";
+import DiscoverPage from "./pages/DiscoverPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/" element={<HomeContent />} />
+        <Route path="/discover" element={<DiscoverPage />} />
         <Route path="/donate" element={<DonatePage />} />
         <Route path="/develop" element={<DevelopPage />} />
+        <Route path="/contribute" element={<DevelopPage />} />
       </Routes>
     </Router>
   );
