b5b23fac9609c1485cdfdd32f7d7caf1099b9c6a
Yuya Iwatsuki <spidywebcoder@gmail.com>
2024-04-13 09:22:40 +0900

    Add /write route

--- react-front-end/secrets.cafe/src/App.tsx
+++ react-front-end/secrets.cafe/src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DonatePage from "@/pages/DonatePage";
 import DevelopPage from "@/pages/DevelopPage";
-import DiscoverPage from "./pages/DiscoverPage";
-import ContributePage from "./pages/ContributePage";
+import DiscoverPage from "@/pages/DiscoverPage";
+import ContributePage from "@/pages/ContributePage";
+import WritePage from "@/pages/WritePage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/donate" element={<DonatePage />} />
         <Route path="/develop" element={<DevelopPage />} />
         <Route path="/contribute" element={<ContributePage />} />
+        <Route path="/write" element={<WritePage />} />
       </Routes>
     </Router>
   );
