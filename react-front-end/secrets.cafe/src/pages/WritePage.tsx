f6ef13ab150bdc3cb2e26229268c42694d66ca23
Yuya Iwatsuki <spidywebcoder@gmail.com>
2024-04-13 18:54:47 +0900

    Fixed an issue where EditPage disappears when reloading the page

--- react-front-end/secrets.cafe/src/pages/WritePage.tsx
+++ react-front-end/secrets.cafe/src/pages/WritePage.tsx
@@ -3,15 +3,22 @@ import Footer from "@/components/Layout/Footer/Footer";
 import Write from "@/components/Features/components/write";
 import EditPage from "@/pages/EditPage";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
-    const [isEditOpen, setIsEditOpen] = useState(false);
+    const [isEditOpen, setIsEditOpen] = useState(() => {
+        const storedIsEditOpen = localStorage.getItem('isEditOpen');
+        return storedIsEditOpen ? JSON.parse(storedIsEditOpen) : false;
+    });
+
+    useEffect(() => {
+        localStorage.setItem('isEditOpen', JSON.stringify(isEditOpen));
+    }, [isEditOpen]);
 
     const handleOpenEdit = () => {
         setIsEditOpen(true);
     }
-
 
     return (
         isEditOpen ? (
@@ -22,7 +29,9 @@ export default function HomePage() {
                 <main className="flex-grow flex flex-col items-center justify-center">
                     <Write />
                     <div className="flex space-x-4 my-8">
-                        <Button size="lg">Rule</Button>
+                        <Link to="/rules">
+                            <Button size="lg">Rule</Button>
+                        </Link>
                         <Button size="lg" onClick={handleOpenEdit}>I Agree</Button>
                     </div>
                 </main>
