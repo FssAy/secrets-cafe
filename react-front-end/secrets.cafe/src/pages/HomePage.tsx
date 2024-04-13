363417fe868271132290bbf5a31fffb43ed51830
Yuya Iwatsuki <spidywebcoder@gmail.com>
2024-04-13 18:59:03 +0900

    Add useEffect to remove the localStorage item to reset the agreement

--- react-front-end/secrets.cafe/src/pages/HomePage.tsx
+++ react-front-end/secrets.cafe/src/pages/HomePage.tsx
@@ -2,9 +2,14 @@ import Navbar from "@/components/Layout/TopNav/navbar";
 import Footer from "@/components/Layout/Footer/Footer";
 import Home from "@/components/Features/components/home";
 import { Button } from "@/components/ui/button";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
+    useEffect(() => {
+        localStorage.removeItem('isEditOpen');
+    }, []);
+
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
